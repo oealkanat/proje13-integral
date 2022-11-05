6b4ae10ea6d1e0f69127b611001b0297fa106343
Osman <oerenalkanat@gmail.com>
2022-11-05 21:58:18 +0300

    fixed the print function, edited scanner messages, reduced lines

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-
-
+    static void polyPrinter(int[] multipler, int degree) {
+        power powSym = new power();
+        for (int j=degree ; j >= 0 ; j--) {
+            if(multipler[j]!=0) {
+                if (multipler[j] > 0 && j!=degree) {
+                    System.out.print("+");
+                }
+                System.out.print(multipler[j]);
+                if (j!=0) {
+                    System.out.print("x" + powSym.myPow(j));
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         power powSym = new power();
         Scanner input = new Scanner(System.in);
         int degree=-1, i;
         int[] multipler;
 
-
-        while (degree < 0) {
-            System.out.print("What's the degree of the equation?: ");
+        while (degree < 0 || degree > 10) {
+            System.out.print("Enter the degree of the polynomial: ");
             degree = input.nextInt();
         }
         multipler = new int[degree+1];
 
-        System.out.println(multipler[0]);
-        for (i = 0 ; i <= degree  ; i++) {
-            System.out.print("What's the multipler of x^" + i + "?: ");
+        for (i = degree ; i >= 0  ; i--) {
+            if(i==0) {
+                System.out.print("Enter the constant (coefficient of x\u2070): ");
+            }
+            if(i==1) {
+                System.out.print("Enter the coefficient of x: ");
+            }
+            if(i!=0 && i!=1) {
+                System.out.print("Enter the coefficient of x" + powSym.myPow(i) + ": ");
+            }
+
             multipler[i] = input.nextInt();
         }
 
         System.out.print("Your polynomial: ");
 
-        for (int j=degree ; j >= 0 ; j--) {
-            if (j != 0) {
-                System.out.print("(" + multipler[j] + "x" + powSym.myPow(j) + ") + ");
-            }
-            if (j == 0) {
-                System.out.print("(" + multipler[j] + ")");
-            }
-        }
-
-        System.out.println("\nIntegral of your polynomial: ");
-
-        for (i = degree ; i >= 0 ; i--) {
-
-        }
-
+        polyPrinter(multipler, degree);
     }
 }
 
